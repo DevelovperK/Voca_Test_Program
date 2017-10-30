4fdce15d119f5c47ade8591248008d87a4e78884
DK_MAX <goodminesss@naver.com>
2017-10-30 19:42:46 +0900

    Create Engine.py

---
+++ Engine.py
@@ -0,0 +1,16 @@
+# _*_ codming:utf-8 _*_
+# this cord is write by python 3.x
+
+voca_num = []
+voca_voc = []
+voca_min = []
+
+Line = []
+DB_path = 'Voca_DB.txt'
+with open(DB_path,'rb') as VDB:
+    while True:
+        line = VDB.readline()
+        if not line:
+			break
+		
+		line = line.
